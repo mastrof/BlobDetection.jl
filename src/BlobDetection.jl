74a1a464ceaa877fdb6e1ffe5a960ac8778cc23a
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-08-28 11:12:34 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.40

---
+++ src/BlobDetection.jl
@@ -0,0 +1,5 @@
+module BlobDetection
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BlobDetection
+using Test
+
+@testset "BlobDetection.jl" begin
+    # Write your tests here.
+end
